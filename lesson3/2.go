3d6b17ad4352d251072aa2862cd979ac7cd5a827
fsdev <fsdev@protonmail.com>
2021-02-26 17:48:34 +0300

    done

---
+++ lesson3/2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+// Написать приложение, которое ищет все простые числа от 0 до N включительно.
+// Число N должно быть задано из стандартного потока ввода. P.S. в math есть подсказка как это сделать
+func main() {
+
+	var n int64
+	_, _ = fmt.Scan(&n)
+
+	var primes []int64
+
+	for i := int64(0); i <= n; i++ {
+		if big.NewInt(i).ProbablyPrime(0) {
+			primes = append(primes, i)
+		}
+	}
+	fmt.Printf("Primes: %v\n", primes)
+}
